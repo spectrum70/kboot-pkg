2c06667fe24f8ef0427acbf63c850d2024d32739
Angelo Dureghello <angelo@kernel-space.org>
2026-01-14 10:44:13 +0100

    add echo to commands
    Signed-off-by: Angelo Dureghello <angelo@kernel-space.org>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+set -x
 echo "copying files to /boot ..."
 sudo cp Build/kboot-pkg/DEBUG_GCC5/X64/kboot.efi /boot/EFI/kboot/kboot_x64.efi
 sudo cp kboot-pkg/kboot/images/kboot.bmp /boot/EFI/kboot/images
-echo "done"
+echo "done."
